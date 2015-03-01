f36e21567c8b2a8367b6fc14053778aa7eff1c77
root <root@Dementia.(none)>
2015-03-01 15:10:18 -0300

    cambios y mas cambios =D

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ app.controller('mainController', function ($scope) {
     //   the current GPS coordinates
     //
     function onSuccess(position) {
-        $scope.mensaje = 'Latitude: '  + position.coords.latitude +'Longitude: ' + position.coords.longitude;
+        $scope.mensaje = 'Latitude: '+position.coords.latitude +'Longitude: ' + position.coords.longitude;
         alert('Latitude: '  + position.coords.latitude + 'Longitude: ' + position.coords.longitude);
     }
 
@@ -19,13 +19,14 @@ app.controller('mainController', function ($scope) {
     function onError(error) {
         $scope.mensaje = 'code: '    + error.code    + '\n' +
               'message: ' + error.message + '\n';
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
     }
 
     // Options: throw an error if no update is received every 30 seconds.
     //
-//    alert(123);
+  //    alert(123);
   //  alert(navigator);
     navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 1000 });
     $scope.mensaje = "sin gps";
-
 });
